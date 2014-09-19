ef2482e6b72673b399d52b3df9008181c352cdd4
Torprendido <urielwel@gmail.com>
2014-09-19 12:52:59 -0500

    Se  hisieron cambios en la clase Arbol

--- Analizador/src/Arbol.java
+++ Analizador/src/Arbol.java
@@ -4,13 +4,32 @@ import java.util.ArrayList;
 public class Arbol {
 
     private Arbol padre;
-    private ArrayList<Arbol> hijos;
+    private final ArrayList<Arbol> hijos;
     private ArrayList<Variable> variables;
     
     public Arbol() {
         padre = new Arbol();
         hijos = new ArrayList();
     }
+
+    public void setHijo(Arbol hijo) {
+        hijo.setPadre(this);
+        hijos.add(hijo);
+    }
     
+    private void setPadre(Arbol padre) {
+        this.padre = padre;
+    }
+
+    public void insertarVariables(Variable var) {
+        variables.add(var);
+    }
     
+    public ArrayList<Arbol> getHijos() {
+        return hijos;
+    }
+    
+    public Arbol getPadre() {
+        return padre;
+    }
 }
